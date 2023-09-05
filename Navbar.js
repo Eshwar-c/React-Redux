b5cfabf2fee7514adc345cb5dd3142fcbc6ba49a
Eshwar-c <108388414+Eshwar-c@users.noreply.github.com>
2023-09-05 14:42:54 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom"
+const navLinkStyle =({isActive})=>{
+  return{
+    fontWeight:isActive ? 'bold ':'normal',
+    TextDecoration :isActive?'none':'underlined',
+    padding:'16px 28px'
+  }
+}
+export const Navbar=()=>{
+  return <div>
+    <NavLink style={navLinkStyle} to='/'>Home</NavLink>
+    <NavLink  style={navLinkStyle}to='about'>About</NavLink>
+  </div>
+}
