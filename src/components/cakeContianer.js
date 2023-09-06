3e4cddbd2fdf8215979b65cfa267c4bc2c4fa169
Eshwar-c <eshwar1652@gmail.com>
2023-09-06 13:03:23 +0530

    practiced async

---
+++ src/components/cakeContianer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function cakeContianer() {
+  return (
+    <div>
+      <h2>NUMBER OF CAKE </h2>
+      <butto>buy cake</butto>
+    </div>
+  )
+}
+
+export default cakeContianer
--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />    
+
   </React.StrictMode>
 );
 
@@ -15,3 +16,51 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+{/*const Buy_Cake='Buy_Cake'
+const Buy_IceCream='Buy_IceCream'
+function buycake(){
+  return{
+    type:'Buy_cake',
+    info:'first redux action'
+  }
+}
+function buyicecream(){
+  return{
+    type:'Buy_IceCream'
+  }
+}
+const cakeInitialState= {
+  numOfCakes:10
+}
+const iceCreamInitialState= {
+  numOfIceCream:20
+}
+const cakeReducer=(state=cakeInitialState,action)=>{
+  switch(action.type){
+    case Buy_Cake:return{...state,
+      numOfCakes:state.numOfCakes-1
+    }
+    default:return state
+  }
+}
+const iceCreamReducer=(state=iceCreamInitialState,action)=>{
+  switch(action.type){
+    case Buy_IceCream:return{...state,
+      numOfIceCream:state.numOfIceCream-1
+    }
+    default:return state
+  }
+}
+const rootReducer=combinedReducers({
+  cake:cakeReducer,
+  iceCream:iceCreamReducer
+})
+const store=createStore(reducer)
+console.log('intial state',store.getState())
+store.unsubscribe=store.subscribe(()=>console.log('updtaed state',store.getState()))
+store.dispatch(buycake())
+store.dispatch(buycake())
+store.dispatch(Buy_IceCream())
+store.unsubscribe()*/}
+
+
